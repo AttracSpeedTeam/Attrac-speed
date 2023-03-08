4864604d0b8995e840276b433159e7bf91cd69be
Gheedor <barthjuju54@gmail.com>
2023-03-08 11:24:14 +0100

    tentative spinner

--- AttrackSpeedApp/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AttrackSpeedApp/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.ViewGroup
 import android.view.Window
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -26,14 +24,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
 
+        val languages = resources.getStringArray(R.array.Languages)
+
+        //create spinner programmatically
+        val spinner = Spinner(this)
+        spinner.layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        val linearLayout = findViewById<LinearLayout>(R.id.linear_layout)
+        //add spinner in linear layout
+        linearLayout?.addView(spinner)
+
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item, languages)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>,
+                                        view: View, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity,
+                    getString(R.string.selected_item) + " " +
+                            "" + languages[position], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
+        }
+
         setContentView(binding.root)
         supportActionBar?.hide()
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.Page_Accueil, R.id.Page_Carte, R.id.Page_Information, R.id.Page_Connexion, R.id.Page_Liste
