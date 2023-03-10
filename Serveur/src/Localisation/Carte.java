ed7988ff8ec17302b459cbee649beb73c4b8c0c5
SIMON Thomas <thomas.simon4@etu.univ-lorraine.fr>
2023-03-10 14:56:24 +0100

    javadoc Carte et Position

--- Serveur/src/Localisation/Carte.java
+++ Serveur/src/Localisation/Carte.java
@@ -5,13 +5,29 @@ import java.util.Map;
 
 import Attraction.Attraction;
 
+/**
+ * classe représentant la carte du parc d'attraction
+ */
 public class Carte {
-	
+
+	/**
+	 * Map des positions de chaque attractions
+	 */
 	public Map<Attraction,Position> attractions;
-	
+
+	/**
+	 * constructeur par defaut, initialisation attribut
+	 */
 	public Carte() {
 		this.attractions = new HashMap<Attraction,Position>();
 	}
+
+	/**
+	 * calcul de la distance en mètres entre 2 attractions par rapport à leur position
+	 * @param a1 attraction 1
+	 * @param a2 attraction 2
+	 * @return distance en mètres
+	 */
 	public int calculerDistance(Attraction a1, Attraction a2) {
 		Position p1 = this.attractions.get(a1);
 		Position p2 = this.attractions.get(a2);
@@ -19,7 +35,13 @@ public class Carte {
 		int y = Math.abs(p1.getPosY()-p2.getPosY());
 		return (int) Math.round(Math.sqrt((x*x)+(y*y)));
 	}
-	
+
+	/*
+	********************
+	* GETTERS ET SETTERS
+	********************
+	*/
+
 	public void ajouterAttraction(Attraction n, Position p) {
 		this.attractions.put(n, p);
 	}
--- Serveur/src/Localisation/Position.java
+++ Serveur/src/Localisation/Position.java
@@ -1,13 +1,25 @@
 package Localisation;
 
+/**
+ * classe representant une position
+ */
 public class Position {
+	/**coordonnée X*/
 	private int posX;
+	/**coordonnée Y*/
 	private int posY;
-	
+
+	/**
+	 * constructeur par defaut
+	 * @param x co X
+	 * @param y co Y
+	 */
 	public Position(int x, int y) {
 		this.posX = x;
 		this.posY = y;
 	}
+
+	/*GETTERS ET SETTERS*/
 
 	public int getPosX() {
 		return posX;
