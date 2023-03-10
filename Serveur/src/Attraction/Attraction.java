5c365b1d10362d714a0462a3c8c0b7a020969142
SIMON Thomas <thomas.simon4@etu.univ-lorraine.fr>
2023-03-10 14:51:27 +0100

    javadoc Attraction

--- Serveur/src/Attraction/Attraction.java
+++ Serveur/src/Attraction/Attraction.java
@@ -2,13 +2,27 @@ package Attraction;
 
 import java.io.Serializable;
 
+/**
+ * classe représentant une attraction
+ */
 public class Attraction implements Serializable {
+	/**nom de l'attraction*/
 	private String nom;
+	/**temps en secondes d'attente actuelle de l'attraction*/
 	private int tempsAttente;
+	/** nombre de places totales de tous les wagons de l'attraction */
 	private int nbPlaces;
+	/**temps en secondes entre chaque wagons de l'attraction*/
 	private int tempsEntreChaqueAttrac;
+	/**longueur en mètres de la file d'attendre*/
 	private int longueurFile;
-	
+
+	/**
+	 * constructeur par defaut d'une attraction
+	 * @param n nom de l'attraction
+	 * @param p nombre de places totales
+	 * @param t temps (s) entre chaque wagons
+	 */
 	public Attraction(String n, int p, int t) {
 		this.nom = n;
 		this.nbPlaces = p;
@@ -17,7 +31,11 @@ public class Attraction implements Serializable {
 		this.tempsAttente = 0;
 	}
 
-	public void calculerTempsAttente() {
+	/**
+	 * actualisation de l'attribut tempsAttente
+	 * par la longueur de la file
+	 */
+	private void calculerTempsAttente() {
 		int longueur = this.longueurFile;
 		int temps = -this.tempsEntreChaqueAttrac;
 		while (longueur > 0) {
@@ -27,11 +45,21 @@ public class Attraction implements Serializable {
 		this.tempsAttente = temps;
 	}
 
+	/**
+	 * mise à jour de la longueur de la file
+	 * puis calcule du temps d'attente
+	 * @param longueurFile
+	 */
 	public void majLongueurFile(int longueurFile) {
 		this.longueurFile = longueurFile;
 		calculerTempsAttente();
 	}
 
+	/*
+	********************
+	* GETTERS ET SETTERS
+	********************
+	*/
 	public String getNom() {
 		return this.nom;
 	}
